e03f55d0285ed766a6aa65aadfd58dfeae44d9cf
IsoqulovaGulbahor <126670618+IsoqulovaGulbahor@users.noreply.github.com>
2023-03-08 16:27:39 +0500

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,13 @@
+<template lang="">
+    <div class="card" id="card">
+    <div class="card-img">
+     <img scr="./ass" alt="">
+    </div>
+    </div>
+</template>
+<script>
+
+</script>
+<style lang="">
+    
+</style>
